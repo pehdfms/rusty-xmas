8c64308703f47ae0e40b5180a40d3f45518fc076
Pedro Henrique Garcia <pehdfms@gmail.com>
2022-08-17 01:34:48 -0300

    Add day 2 solution (#5)
    Co-authored-by: Pedro Henrique Garcia <pedro@projetodigital.com.br>

---
+++ src/solves/y2019/days/day2.rs
@@ -0,0 +1,50 @@
+use crate::solves::year::AdventOfCodeDay;
+
+use super::intcode::IntcodeComputer;
+
+fn parse_memory(data: String) -> Vec<i32> {
+    data.trim()
+        .split(',')
+        .map(|n| n.parse().expect("All splits should parse as number."))
+        .collect::<Vec<i32>>()
+}
+
+fn part1(data: String) -> String {
+    let memory = parse_memory(data);
+    let mut computer = IntcodeComputer::new(memory);
+
+    computer.replace(1, 12);
+    computer.replace(2, 2);
+
+    while computer.step() {}
+
+    computer.read(0).to_string()
+}
+
+fn part2(data: String) -> String {
+    let memory = parse_memory(data);
+    let mut computer = IntcodeComputer::new(memory);
+
+    for noun in 0..=99 {
+        for verb in 0..=99 {
+            computer.replace(1, noun);
+            computer.replace(2, verb);
+
+            while computer.step() {}
+
+            if computer.read(0) == 19690720 {
+                return (100 * noun + verb).to_string();
+            }
+
+            computer.reset();
+        }
+    }
+
+    panic!("Couldn't find result in sample!");
+}
+
+pub const SOLUTION: AdventOfCodeDay = AdventOfCodeDay {
+    name: "1202 Program Alarm",
+    part1: Some(part1),
+    part2: Some(part2),
+};
