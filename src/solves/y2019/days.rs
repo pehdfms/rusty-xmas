2af0081c139fe5d1223c2830a4d81277d0f807c7
Pedro Henrique Garcia <pedro@projetodigital.com.br>
2022-08-20 14:10:52 -0300

    feat: add 2019d3p1 solution

--- src/solves/y2019/days.rs
+++ src/solves/y2019/days.rs
@@ -2,9 +2,10 @@ use crate::solves::year::AdventOfCodeDay;
 
 mod day1;
 mod day2;
+mod day3;
 
 mod intcode;
 
 pub fn get_days() -> Vec<AdventOfCodeDay> {
-    vec![day1::SOLUTION, day2::SOLUTION]
+    vec![day1::SOLUTION, day2::SOLUTION, day3::SOLUTION]
 }
